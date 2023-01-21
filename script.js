7034a56ab1372ca30fa904c5d430db6b223d7bfe
Charlie Hodgkinson <charlotte.hodgkinson4@gmail.com>
2023-01-21 21:31:18 +0000

    add flowers that spin

--- script.js
+++ script.js
@@ -37,8 +37,53 @@ class Root {
       ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
       ctx.fillStyle = 'hsl(140,100%,' + this.lightness + '%)';
       ctx.fill();
-      // ctx.stroke(); // gives it border, if none border will be black
+      ctx.stroke(); // gives it border, if none border will be black
       requestAnimationFrame(this.update.bind(this));
+    } else {
+      const flower = new Flower(this.x, this.y, this.size);
+      flower.grow();
+    }
+  }
+}
+
+class Flower {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.vs = Math.random() * 0.3 + 0.2;
+    this.maxFlowerSize = this.size + Math.random() * 100;
+    this.image = new Image();
+    this.image.src = 'flowers.png';
+    this.frameSize = 100;
+    this.frameY = Math.floor(Math.random() * 3); // 0, 1 or 2
+    this.frameX = Math.floor(Math.random() * 3); // 0, 1 or 2
+    this.willFlower = this.size > 11.5 ? true : false;
+    this.angle = 0;
+    this.va = Math.random() * 0.05 - 0.025;
+
+  };
+  grow() {
+    if ( this.size < this.maxFlowerSize && this.willFlower ) {
+      this.size += this.vs;
+      this.angle += this.va;
+
+      ctx.save(); // saves canvas settings
+      ctx.translate(this.x, this.y); // sets the 0, 0 to x and y
+      ctx.rotate(this.angle);
+      ctx.drawImage(
+        this.image,
+        this.frameSize * this.frameX, // start of image x
+        this.frameSize * this.frameY, // start of image y
+        this.frameSize, // size width
+        this.frameSize, // size height
+        0 - this.size/2, // x location to draw
+        0 - this.size/2, // y location to draw
+        this.size, // size when drawn x
+        this.size // size when drawn y
+      );
+      ctx.restore(); // restores canvas settings to what was saved
+      requestAnimationFrame(this.grow.bind(this))
     }
   }
 }
